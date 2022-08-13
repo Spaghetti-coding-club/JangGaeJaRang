72138bf2457834ddc592a5197cb9697318f6d3ba
baegteun <baegteun@gmail.com>
2022-08-13 14:20:16 +0900

    Initial Commit

---
+++ JangGaeJaRang/JangGaeJaRangApp.swift
@@ -0,0 +1,17 @@
+//
+//  JangGaeJaRangApp.swift
+//  JangGaeJaRang
+//
+//  Created by 최형우 on 2022/08/13.
+//
+
+import SwiftUI
+
+@main
+struct JangGaeJaRangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
