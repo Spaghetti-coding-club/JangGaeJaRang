7f7c98244cbebfe0f2b67c2c518b2c10ccd7fb00
baegteun <baegteun@gmail.com>
2022-08-14 01:53:53 +0900

    :sparkles: :: AppleSigninLogic

--- App/Sources/Presentation/Scenes/Signin/SigninVM.swift
+++ App/Sources/Presentation/Scenes/Signin/SigninVM.swift
@@ -1,5 +1,32 @@
-
+import Foundation
 
 final class SigninVM: BaseViewModel {
+    private let authService = AuthService.shared
+    var nonce: String? = nil
+    @Published var sceneState = SceneState.signin
     
+    enum Input {
+        case appleSigninCompleted(cred: Data)
+    }
+    
+    func send(_ input: Input) {
+        switch input {
+        case let .appleSigninCompleted(cred):
+            appleSigninCompleted(cred: cred)
+        }
+    }
+    
+    private func appleSigninCompleted(cred: Data) {
+        guard let idToken = String(data: cred, encoding: .utf8) else { return }
+        guard let nonce = nonce else { return }
+        
+        Task {
+            do {
+                let isInitialSignin = try await authService.signin(idToken: idToken, nonce:nonce)
+                sceneState = isInitialSignin ? .signup : .main
+            } catch {
+                isErrorOcuured = true
+            }
+        }
+    }
 }
