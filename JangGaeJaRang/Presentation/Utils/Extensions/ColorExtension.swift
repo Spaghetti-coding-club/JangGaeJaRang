fe9d0519429a85978aec385b2bb032e8643cc346
최정현 <choi.jeonghyun@mintech.kr>
2022-08-13 19:14:27 +0900

    feat: ColorExtension 생성

---
+++ JangGaeJaRang/Presentation/Utils/Extensions/ColorExtension.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+extension Color {
+    static var background: Color { Color("background") }
+    static var defaultGreen: Color { Color("defaultGreen") }
+    static var disabledGreen: Color { Color("disabledGreen") }
+    static var gray1: Color { Color("gray1") }
+    static var gray2: Color { Color("gray2") }
+    static var gray3: Color { Color("gray3") }
+    static var gray4: Color { Color("gray4") }
+    static var gray5: Color { Color("gray5") }
+    static var gray6: Color { Color("gray6") }
+    static var gray7: Color { Color("gray7") }
+}
