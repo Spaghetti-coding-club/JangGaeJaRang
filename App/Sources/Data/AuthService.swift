d430b4812ea7242b42b7af365912b794400d3ab3
baegteun <baegteun@gmail.com>
2022-08-14 01:21:29 +0900

    feat: AuthService Signin

--- App/Sources/Data/AuthService.swift
+++ App/Sources/Data/AuthService.swift
@@ -1,1 +1,17 @@
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
+struct AuthService {
+    static let shared = AuthService()
+    func signin(idToken: String, nonce: String) async throws -> Bool {
+        let cred = OAuthProvider.credential(
+            withProviderID: "apple.com",
+            idToken: idToken,
+            rawNonce: nonce
+        )
+        guard let res = try? await Auth.auth().signIn(with: cred) else { throw JGJRError.signinIsFailed }
+        guard let user = try? await Firestore.firestore().collection("User").document(res.user.uid).getDocument() else { throw JGJRError.signinIsFailed }
+        return (user.data()?["isFirst"] as? Bool ?? true) == true
+    }
+}
